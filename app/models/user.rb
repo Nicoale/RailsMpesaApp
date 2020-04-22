bd4f626503cd424fa8d18594c39a65f465935d23
nicoale <nicoale8786@gmail.com>
2020-04-22 08:20:21 +0300

    validated user input and ensured no duplicationof entries

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,8 +1,8 @@
 class User < ApplicationRecord
-    validates :first_name, presence: true
-    validates :last_name, presence: true
-    validates :phone_number, presence: true 
-    validates :id_number, presence: true
-    validates :password, presence: true
-  end
+    validates :first_name, presence: true, length:{maximum: 30},format: { with: /\A[a-zA-Z]+\z/}
+    validates :last_name, presence: true, length:{maximum: 30}, format: { with: /\A[a-zA-Z]+\z/}
+    validates :phone_number, presence: true, uniqueness: true, length: {in: 10..13},format: { with: /\A[a-zA-Z]+\z/}
+    validates :id_number, presence: true, uniqueness: true, length: {in: 7..9}
+    validates :password, presence: true,  length:{minimum: 8}
+    validates :pin, presence: true,length:{is: 4 }
 end
