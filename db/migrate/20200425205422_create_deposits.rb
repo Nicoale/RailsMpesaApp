7c20c6b62ea70e5d16e0eb2b8d0aa89ed5e69741
nicoale <nicoale8786@gmail.com>
2020-04-27 05:36:26 +0300

    unit tests for deposits

---
+++ db/migrate/20200425205422_create_deposits.rb
@@ -0,0 +1,12 @@
+class CreateDeposits < ActiveRecord::Migration[6.0]
+  def change
+    create_table :deposits do |t|
+      t.string :phone_number, null: false
+      t.float :new_deposit, null: false, default: 0.0
+      t.float :balance, null: false, default: 0.0
+      t.integer :pin, null: false, default: 1234
+
+      t.timestamps
+    end
+  end
+end
---
+++ spec/factories/deposits.rb
@@ -0,0 +1,8 @@
+FactoryBot.define do
+  factory :deposit do
+    phone_number { "021345678" }
+    new_deposit { 1.5 }
+    balance { 1.5 }
+    pin { 1234 }
+  end
+end
