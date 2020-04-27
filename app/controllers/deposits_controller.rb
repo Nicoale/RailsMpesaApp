be4114758a5af59b05c1ab7fe5bdfe7f0a7ab14e
nicoale <nicoale8786@gmail.com>
2020-04-27 06:15:30 +0300

    integration tests for deposits

---
+++ app/controllers/deposits_controller.rb
@@ -0,0 +1,20 @@
+class  DepositsController < ApplicationController
+    def new
+    end
+    def create
+      @deposit = deposit.new(deposit_params)
+      if @deposit.save
+        redirect_to deposit_path(@deposit)
+      else
+        render 'new'
+      end
+    end
+    def show
+      @deposit = deposit.find(params[:phone_number])
+    end
+  
+    private
+    def deposit_params
+      params.require(:deposit).permit( :phone_number, :new_deposit,:balance, :pin )
+    end
+  end
