ab699f34fb5efb8398423e7e8d288cf0901d4c75
nicoale <nicoale8786@gmail.com>
2020-04-28 12:17:07 +0300

    added the landing page for the app

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  get 'statistics/home'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   get 'users/new', to: 'users#new', as: 'new_user'
   post 'users/', to: 'users#create'
@@ -6,4 +7,5 @@ Rails.application.routes.draw do
   get 'deposit/:phone_number', to: 'deposits#show', as: 'deposits'
   get 'withdrawal/:phone_number', to: 'withdrawal#show', as: 'withdrawals'
   get 'send/:phone_number', to: 'send#show', as: 'sends'
+  root to:'statistics#home'
 end
