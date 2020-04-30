3d7404f8e078b83245311122d34a8e724df0b921
nicoale <nicoale8786@gmail.com>
2020-04-30 09:29:35 +0300

    added sending email/text option to the new user

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,7 +3,12 @@ class UsersController < ApplicationController
     end
     def create
         @user = User.new(user_params)
+        respond_to do |format|
         if @user.save
+          UserMailer.with(user: @user).welcome_email.deliver_later
+          format.html { redirect_to(@user, notice: 'Your account was successfully created.') }
+          format.json { render json: @user, status: :created, location: @user }
+          
           redirect_to user_path(@user)
         else
           render 'new'
